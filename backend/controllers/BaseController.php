52299a45ae88563a80cb32e34f44f90550c548a9
penngo <penngo@163.com>
2016-08-10 22:50:57 +0800

    no message

--- backend/controllers/BaseController.php
+++ backend/controllers/BaseController.php
@@ -40,21 +40,21 @@ class BaseController extends Controller
                     header("Content-type: text/html; charset=utf-8");
                     exit('没有权限访问'.$route);
                 }
-//                 $rights = $system_rights[$route];
-//                 if($route != 'system-log/index'){
-//                     $systemLog = new SystemLog();
-//                     $systemLog->url = $route;
-//                     $systemLog->controller_id = $action->controller->id;
-//                     $systemLog->action_id = $action->id;
-//                     $systemLog->module_name = $rights['module_name'];
-//                     $systemLog->func_name = $rights['func_name'];
-//                     $systemLog->right_name = $rights['right_name'];
-//                     $systemLog->create_date = date('Y-m-d H:i:s');
-//                     $systemLog->create_user = Yii::$app->user->identity->uname;
-//                     $systemLog->client_ip = CommonFun::getClientIp();
+                $rights = $system_rights[$route];
+                if($route != 'system-log/index'){
+                    $systemLog = new SystemLog();
+                    $systemLog->url = $route;
+                    $systemLog->controller_id = $action->controller->id;
+                    $systemLog->action_id = $action->id;
+                    $systemLog->module_name = $rights['module_name'];
+                    $systemLog->func_name = $rights['func_name'];
+                    $systemLog->right_name = $rights['right_name'];
+                    $systemLog->create_date = date('Y-m-d H:i:s');
+                    $systemLog->create_user = Yii::$app->user->identity->uname;
+                    $systemLog->client_ip = CommonFun::getClientIp();
                     
-//                     $systemLog->save();
-//                 }
+                    $systemLog->save();
+                }
             }
            
         }
