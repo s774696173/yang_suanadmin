6349a2e652546a78eaf2135a5f26600cba1a47be
penngo@163.com <penngo@163.com>
2016-08-16 10:40:39 +0800

    no message

--- backend/config/main.php
+++ backend/config/main.php
@@ -13,7 +13,6 @@ $config = [
     'modules' => [],
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'w3BnewAWmCrjijzkiLucYD5Ty1Ym_V9F',
         ],
         /*
