b16be79bc5785b8f87c2735062ecd537d11ef491
penngo <penngo@163.com>
2016-09-20 12:23:10 +0800

    no message

--- common/utils/CommonFun.php
+++ common/utils/CommonFun.php
@@ -102,17 +102,7 @@ class CommonFun extends Object{
         //echo implode ( "", array_reverse ( $lines ) );
         return array_reverse ( array_slice ( $lines, 0, $n ) );
     }
-    /**
-     * 将字符串转换为数组
-     *
-     * @param  string  $data 字符串
-     * @return array 返回数组格式，如果，data为空，则返回空数组
-     */
-    function string2array($data) {
-        if($data == '') return array();
-        @eval("\$array = $data;");
-        return $array;
-    }
+   
     
     public static function sortClass($orderby, $key){
         $data = explode(' ', $orderby);
